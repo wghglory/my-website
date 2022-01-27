a49a2c2c426b6dd2112d2bb894e17004cbb2f131
Guanghui Wang <guanghui-wang@foxmail.com>
2022-01-27 10:59:34 +0800

    refactor: move files out of src, the default structure of nextjs

--- __test__/index.test.tsx
+++ __test__/index.test.tsx
@@ -1,6 +1,6 @@
 import {render, screen} from '@testing-library/react';
 
-import Home from '../index';
+import Home from '../pages/index';
 
 describe('Home', () => {
   it('renders a heading', () => {
