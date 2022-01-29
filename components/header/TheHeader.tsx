5f442b6ba5b57e5a04da8db09de13ff043cd7ffa
Guanghui Wang <guanghui-wang@foxmail.com>
2022-01-29 13:04:26 +0800

    fix(header): extract AppLogo and fix focus trap on header menu, fix overflow set by useMedia

--- components/header/TheHeader.tsx
+++ components/header/TheHeader.tsx
@@ -2,16 +2,20 @@
 // 2. for performance, not using resize and mediaquery to setShowMenu false when the browser window width increases. A little weird thing happen if opening the menu in a small device and then drag to wide device and then drag back. The menu appears. But it's acceptable. https://www.pluralsight.com/guides/re-render-react-component-on-window-resize
 
 import cn from 'classnames';
+import FocusTrap from 'focus-trap-react';
 import Link from 'next/link';
 import {useEffect, useState} from 'react';
+import {useMedia} from 'react-use';
 
 import PlaneIcon from '/public/telegram-plane.svg';
 
+import AppLogo from '../AppLogo';
 import MenuButton from './MenuButton';
 import ThemeChanger from './ThemeChanger';
 
 export default function TheHeader() {
   const [showMenu, setShowMenu] = useState(false);
+  const isLg = useMedia('(min-width: 1024px)');
 
   // ECS to quit small nav menu
   // function keyPress(e: React.KeyboardEvent) {
@@ -35,115 +39,111 @@ export default function TheHeader() {
     };
   }, [setShowMenu]);
 
-  // disable scroll when opening
+  // disable scroll when opening and when in a small screen
   useEffect(() => {
-    if (showMenu) {
-      document.documentElement.style.overflow = 'hidden';
-    } else {
-      document.documentElement.style.overflow = 'auto';
-    }
-  }, [showMenu]);
+    document.documentElement.style.overflow = showMenu && !isLg ? 'hidden' : 'auto';
+  }, [showMenu, isLg]);
 
   return (
-    // h-screen + body overflow hidden to display the menu in the screen
-    <header className={cn({'h-screen': showMenu}, 'flex flex-col bg-gray-100 dark:bg-gray-900 lg:h-auto')}>
-      <div className="container mx-auto flex h-20 items-center justify-between px-6">
-        <h1>
-          <Link href="/">
-            <a className="text-lg font-bold md:text-xl lg:text-2xl">Derek Wang</a>
-          </Link>
-        </h1>
-        <nav className="hidden lg:block">
-          <ul className="flex gap-x-4 sm:gap-x-6 md:gap-x-8">
-            <li className="hover:text-orange-600">
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-            </li>
-            <li className="hover:text-orange-600">
-              <Link href="#about">
-                <a>About</a>
-              </Link>
-            </li>
-            <li className="hover:text-orange-600">
-              <Link href="#projects">
-                <a>Projects</a>
-              </Link>
-            </li>
-            <li className="hover:text-orange-600">
-              <Link href="#blog">
-                <a>Blog</a>
-              </Link>
-            </li>
-            <li className="hover:text-orange-600">
-              <Link href="/snippets">
-                <a>Snippets</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="hidden items-center gap-4 lg:flex">
-          <ThemeChanger />
-          <button className="btn-primary">
-            Contact
-            <PlaneIcon className="w-5" />
-          </button>
-        </div>
-        <div className="inline-flex lg:hidden">
-          <MenuButton
-            onClick={() => {
-              setShowMenu(!showMenu);
-            }}
-          />
-        </div>
-      </div>
-
-      {
-        /* Small screen menu open!!! Menu open: "block", Menu closed: "hidden" */
-        // <div
-        //   tabIndex={0}
-        //   className={`flex flex-1 flex-col gap-4 overflow-auto lg:hidden ${showMenu ? 'flex' : 'hidden'}`}
-        //   onKeyUp={keyPress}
-        // >
-        <div className={`flex flex-1 flex-col gap-4 overflow-auto lg:hidden ${showMenu ? 'flex' : 'hidden'}`}>
-          <nav className="mt-auto">
-            <ul>
-              <li className="py-3 text-center hover:bg-gray-100 hover:text-orange-600 hover:dark:bg-gray-800 md:py-5">
+    <FocusTrap active={showMenu && !isLg}>
+      {/* // h-screen + body overflow hidden to display the menu in the screen */}
+      <header className={cn({'h-screen': showMenu}, 'flex flex-col bg-gray-100 dark:bg-gray-900 lg:h-auto')}>
+        <div className="container mx-auto flex h-20 items-center justify-between px-6">
+          <h1 aria-label="Derek Wang's Website">
+            <AppLogo />
+          </h1>
+          <nav className="hidden lg:block">
+            <ul className="flex gap-x-4 sm:gap-x-6 md:gap-x-8">
+              <li className="hover:text-orange-600">
                 <Link href="/">
                   <a>Home</a>
                 </Link>
               </li>
-              <li className="py-3 text-center hover:bg-gray-100 hover:text-orange-600 hover:dark:bg-gray-800 md:py-5">
+              <li className="hover:text-orange-600">
                 <Link href="#about">
                   <a>About</a>
                 </Link>
               </li>
-              <li className="py-3 text-center hover:bg-gray-100 hover:text-orange-600 hover:dark:bg-gray-800 md:py-5">
+              <li className="hover:text-orange-600">
                 <Link href="#projects">
                   <a>Projects</a>
                 </Link>
               </li>
-              <li className="py-3 text-center hover:bg-gray-100 hover:text-orange-600 hover:dark:bg-gray-800 md:py-5">
+              <li className="hover:text-orange-600">
                 <Link href="#blog">
                   <a>Blog</a>
                 </Link>
               </li>
-              <li className="py-3 text-center hover:bg-gray-100 hover:text-orange-600 hover:dark:bg-gray-800 md:py-5">
+              <li className="hover:text-orange-600">
                 <Link href="/snippets">
                   <a>Snippets</a>
                 </Link>
               </li>
             </ul>
           </nav>
-          <div className="mb-auto flex flex-col items-center gap-4 border-t border-gray-200 py-8 dark:border-gray-700">
+          <div className="hidden items-center gap-4 lg:flex">
             <ThemeChanger />
             <button className="btn-primary">
               Contact
               <PlaneIcon className="w-5" />
             </button>
           </div>
+          <div className="inline-flex lg:hidden">
+            <MenuButton
+              onClick={() => {
+                setShowMenu(!showMenu);
+              }}
+            />
+          </div>
         </div>
-      }
-    </header>
+
+        {
+          /* Small screen menu open!!! Menu open: "block", Menu closed: "hidden" */
+          // <div
+          //   tabIndex={0}
+          //   className={`flex flex-1 flex-col gap-4 overflow-auto lg:hidden ${showMenu ? 'flex' : 'hidden'}`}
+          //   onKeyUp={keyPress}
+          // >
+          <div className={`flex flex-1 flex-col gap-4 overflow-auto lg:hidden ${showMenu ? 'flex' : 'hidden'}`}>
+            <nav className="mt-auto">
+              <ul>
+                <li className="py-3 text-center hover:bg-gray-100 hover:text-orange-600 hover:dark:bg-gray-800 md:py-5">
+                  <Link href="/">
+                    <a>Home</a>
+                  </Link>
+                </li>
+                <li className="py-3 text-center hover:bg-gray-100 hover:text-orange-600 hover:dark:bg-gray-800 md:py-5">
+                  <Link href="#about">
+                    <a>About</a>
+                  </Link>
+                </li>
+                <li className="py-3 text-center hover:bg-gray-100 hover:text-orange-600 hover:dark:bg-gray-800 md:py-5">
+                  <Link href="#projects">
+                    <a>Projects</a>
+                  </Link>
+                </li>
+                <li className="py-3 text-center hover:bg-gray-100 hover:text-orange-600 hover:dark:bg-gray-800 md:py-5">
+                  <Link href="#blog">
+                    <a>Blog</a>
+                  </Link>
+                </li>
+                <li className="py-3 text-center hover:bg-gray-100 hover:text-orange-600 hover:dark:bg-gray-800 md:py-5">
+                  <Link href="/snippets">
+                    <a>Snippets</a>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <div className="mb-auto flex flex-col items-center gap-4 border-t border-gray-200 py-8 dark:border-gray-700">
+              <ThemeChanger />
+              <button className="btn-primary">
+                Contact
+                <PlaneIcon className="w-5" />
+              </button>
+            </div>
+          </div>
+        }
+      </header>
+    </FocusTrap>
   );
 }
