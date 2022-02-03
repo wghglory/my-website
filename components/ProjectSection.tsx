e146773e72edd2bb2896bcb5449a21e5bbd76b97
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-03 23:02:07 +0800

    feat: add project section

---
+++ components/ProjectSection.tsx
@@ -0,0 +1,10 @@
+export default function ProjectSection() {
+  return (
+    <section className="bg-gray-100 dark:bg-gray-900">
+      <div className="container m-auto px-6 py-6 lg:py-12">
+        <h2 className="text-2xl lg:py-6 lg:text-5xl">Projects</h2>
+        <div></div>
+      </div>
+    </section>
+  );
+}
