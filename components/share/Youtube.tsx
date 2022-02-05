8b29fea8e2f58d6620dff1cf5e779afd94295d13
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-05 20:38:47 +0800

    feat: post style using @tailwindcss/typography

--- components/share/Youtube.tsx
+++ components/share/Youtube.tsx
@@ -1,11 +1,11 @@
 export default function YouTube({id}: {id: string}) {
   return (
-    <div className="relative hidden h-0 pb-9">
+    <div className="relative h-0 max-w-full overflow-hidden pb-[56.25%]">
       <iframe
         src={`https://www.youtube.com/embed/${id}`}
         allow="autoplay; encrypted-media"
         title="Embedded YouTube video"
-        className="absolute top-0 left-0 h-full w-full"
+        className="absolute inset-0 h-full w-full"
       />
     </div>
   );
