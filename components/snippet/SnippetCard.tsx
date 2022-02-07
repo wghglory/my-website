226dc332a3428f5585226ccb2fae95d0208f25d7
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-07 13:08:39 +0800

    style: card border

--- components/snippet/SnippetCard.tsx
+++ components/snippet/SnippetCard.tsx
@@ -4,7 +4,7 @@ import type {FileMeta} from '@/models';
 
 export default function SnippetCard({snippet}: {snippet: FileMeta}) {
   return (
-    <div className="flex flex-col rounded-xl border-2 border-transparent bg-white shadow-md hover:border-king-500 hover:shadow-xl dark:bg-gray-900">
+    <div className="flex flex-col rounded-xl border border-gray-200 bg-white hover:border-king-500 dark:border-gray-700 dark:bg-gray-800 dark:hover:border-king-400">
       <div className="flex flex-1 flex-col gap-4 px-8 py-6">
         <div className="flex-1 space-y-6">
           <header className="text-2xl font-semibold lg:text-center">
