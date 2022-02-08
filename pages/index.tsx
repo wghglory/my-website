720e73d546957009c4c04b01822e7ab2f137fd32
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-08 21:21:55 +0800

    feat: projects in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
+import {GetStaticProps} from 'next';
+
 import ExperienceSection from '@/components/experience/ExperienceSection';
 import HeroSection from '@/components/hero/HeroSection';
 import ProjectSection from '@/components/project/ProjectSection';
+import {getAllFiles} from '@/lib/file';
+import {FileMeta} from '@/models';
 
-export default function HomePage() {
+export default function IndexPage({projects}: {projects: FileMeta[]}) {
   return (
     <main>
       <HeroSection />
       <ExperienceSection />
-      <ProjectSection />
+      <ProjectSection projects={projects} />
     </main>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const projects = getAllFiles('projects')
+    .slice(0, 6)
+    .map((f) => f.meta);
+
+  return {props: {projects}};
+};
