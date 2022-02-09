6d6924118f98b3ea13f23eb83dfaaba10af2670c
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-09 13:46:41 +0800

    style: use image instead img in hero section

--- components/hero/HeroSection.tsx
+++ components/hero/HeroSection.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import DoorBg from '/public/door.svg';
 import UMLogo from '/public/um.svg';
 import UnionbankLogo from '/public/unionbank.svg';
@@ -11,7 +13,7 @@ export default function HeroSection() {
     <section className="bg-white dark:bg-gray-900" id="home">
       <div className="container m-auto px-6 lg:pt-12">
         <div className="flex flex-col justify-center gap-10 lg:flex-row lg:gap-20 xl:gap-28">
-          <div className="flex max-w-5xl flex-col justify-center gap-6">
+          <div className="flex w-full max-w-5xl flex-col justify-center gap-6 lg:w-2/3 lg:py-10 xl:py-20">
             <div>
               <h2 className="py-6 text-center text-2xl lg:text-left lg:text-5xl">
                 Web Developer Loving <span className="text-sky-500">React</span> and{' '}
@@ -26,21 +28,14 @@ export default function HeroSection() {
               <SocialIcons />
             </div>
           </div>
-          <div className="relative flex justify-center text-center text-queen-300 dark:text-gray-600">
-            <style jsx>
-              {`
-                .door {
-                  background: url(/door.svg) no-repeat center;
-                  background-size: contain;
-                }
-              `}
-            </style>
+          <div className="relative flex min-h-[250px] w-full justify-center text-queen-300 dark:text-gray-600 lg:w-1/3">
             <DoorBg className="absolute h-full" />
-            <img
+            <Image src="/me.png" alt="Guanghui Picture" layout="fill" objectFit="contain" />
+            {/* <img
               src="/me.png"
               className="z-[1] max-h-[300px] min-h-[200px] w-auto lg:max-h-[400px]"
               alt="Guanghui Picture"
-            />
+            /> */}
           </div>
         </div>
       </div>
