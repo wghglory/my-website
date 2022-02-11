22393b579806e87117b95a3c49b96951bd2775e6
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-11 23:22:10 +0800

    feat: add education

--- components/share/SectionLayout.tsx
+++ components/share/SectionLayout.tsx
@@ -2,15 +2,15 @@ export default function SectionLayout({
   children,
   title,
   id,
-  bgClass = 'bg-gray-100 dark:bg-gray-800',
+  className = 'bg-gray-100 dark:bg-gray-800',
 }: {
   children: React.ReactNode;
   title: string;
   id?: string;
-  bgClass?: string;
+  className?: string;
 }) {
   return (
-    <section className={bgClass} id={id}>
+    <section className={className} id={id}>
       <div className="container mx-auto my-10 space-y-6 px-6 sm:px-14 lg:my-20 xl:px-6">
         <header className="mb-10 flex flex-col gap-6 text-center sm:mb-14 md:flex-row md:justify-between">
           <h2 className="text-3xl lg:text-left lg:text-5xl">{title}</h2>
