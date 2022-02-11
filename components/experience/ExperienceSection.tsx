f3bb0238fa2b01db65287708c470d38fa65ac4ee
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-11 23:28:26 +0800

    style: header size

--- components/experience/ExperienceSection.tsx
+++ components/experience/ExperienceSection.tsx
@@ -12,7 +12,7 @@ export default function ExperienceSection() {
     <section className="bg-white dark:bg-gray-900" id="experience">
       <div className="container m-auto px-6 py-6 lg:py-16">
         <div className="flex flex-col justify-between sm:flex-row">
-          <h2 className="flex items-center justify-center text-2xl sm:justify-start lg:py-6 lg:text-5xl">
+          <h2 className="flex items-center justify-center text-3xl sm:justify-start lg:py-6 lg:text-5xl">
             <strong className="py-6 text-4xl text-queen-500 lg:text-6xl">10</strong>&nbsp; Year Experience
           </h2>
           <div className="flex justify-center">
