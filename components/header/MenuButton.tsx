ec43dc666b01c3ded1ddeed034a784cb8de6fd53
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-14 15:01:05 +0800

    feat: svg path animate

--- components/header/MenuButton.tsx
+++ components/header/MenuButton.tsx
@@ -1,22 +1,32 @@
+import {motion} from 'framer-motion';
+
+import {svgVariants} from '@/lib/motion';
+
 export default function MenuButton({onClick}: {onClick: () => void}) {
   return (
     <button onClick={onClick}>
-      <svg className="group h-10 w-10" viewBox="0 0 56 56" fill="none">
-        <circle
+      <motion.svg initial={false} className="group h-10 w-10" viewBox="0 0 56 56" fill="none">
+        <motion.circle
+          initial="start"
+          animate="end"
+          variants={svgVariants}
           cx="28"
           cy="28"
           r="27"
           className="stroke-gray-300 group-hover:stroke-gray-900 dark:stroke-gray-600 dark:group-hover:stroke-gray-100"
           strokeWidth="2"
         />
-        <path
+        <motion.path
+          initial="start"
+          animate="end"
+          variants={svgVariants}
           d="M17.8182 35.6364H38.1818M17.8182 20.3636H38.1818H17.8182ZM17.8182 28H38.1818H17.8182Z"
           stroke="currentColor"
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
         />
-      </svg>
+      </motion.svg>
     </button>
   );
 }
