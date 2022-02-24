d220242ebfd3519318f172732ac5418400a0637a
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-24 09:41:03 +0800

    refactor: app card component and image cover

--- components/share/AppCardCover.tsx
+++ components/share/AppCardCover.tsx
@@ -1,7 +1,14 @@
 export default function AppCardCover({cover_image}: {cover_image: string}) {
   return (
     <>
-      <div className={`img-bg h-48 rounded-t-xl bg-cover bg-center bg-no-repeat`}>{/* https://hypercolor.dev/ */}</div>
+      <div className={`img-bg relative overflow-hidden pb-[56.25%]`}>
+        {/* https://hypercolor.dev/ */}
+        <img
+          src={cover_image}
+          className="absolute h-full w-full object-cover duration-300 group-hover:scale-105"
+          alt="cover image"
+        />
+      </div>
       <style jsx>
         {`
           .img-bg {
