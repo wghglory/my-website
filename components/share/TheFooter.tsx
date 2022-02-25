125b682dae8a9b6c49a6a4271e1b27c5e5530eff
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-25 23:06:24 +0800

    style: footer center

--- components/share/TheFooter.tsx
+++ components/share/TheFooter.tsx
@@ -11,7 +11,7 @@ export default function TheFooter() {
     <footer className="border-t border-gray-200/80 bg-gray-200 pt-20 pb-10 dark:border-gray-700/80 dark:bg-gray-700">
       <div className="container mx-auto px-6">
         <div className="grid grid-cols-1 gap-20 text-center lg:grid-cols-2 lg:text-left">
-          <section>
+          <section className="lg:pl-[50%]">
             <AppLogo />
             <p>Staff Engineer in VMware</p>
           </section>
@@ -28,7 +28,7 @@ export default function TheFooter() {
               </Link>
             </div>
           </section>
-          <section>
+          <section className="lg:pl-[50%]">
             <p>
               Learn, think, share, collaborate. <br />
               Make a progress every day.
@@ -38,36 +38,48 @@ export default function TheFooter() {
             <div className="text-xl font-bold">Sitemap</div>
             <nav>
               <ul>
-                <li className="py-3 text-center hover:font-semibold dark:hover:text-gray-100  md:py-1 lg:text-left">
+                <li className="py-3 text-center md:py-1 lg:text-left">
                   <Link href="/">
-                    <a className="pb-1 hover:border-b-2 hover:border-gray-900 dark:hover:border-gray-100">Home</a>
+                    <a className="pb-1 hover:border-b-2 hover:border-gray-900 hover:font-semibold dark:hover:border-gray-100 dark:hover:text-gray-100">
+                      Home
+                    </a>
                   </Link>
                 </li>
-                <li className="py-3 text-center hover:font-semibold dark:hover:text-gray-100 md:py-1 lg:text-left">
+                <li className="py-3 text-center md:py-1 lg:text-left">
                   <Link href="/about">
-                    <a className="pb-1 hover:border-b-2 hover:border-gray-900 dark:hover:border-gray-100">About</a>
+                    <a className="pb-1 hover:border-b-2 hover:border-gray-900 hover:font-semibold dark:hover:border-gray-100 dark:hover:text-gray-100">
+                      About
+                    </a>
                   </Link>
                 </li>
-                <li className="py-3 text-center hover:font-semibold dark:hover:text-gray-100 md:py-1 lg:text-left">
+                <li className="py-3 text-center md:py-1 lg:text-left">
                   <Link href="/#project">
-                    <a className="pb-1 hover:border-b-2 hover:border-gray-900 dark:hover:border-gray-100">Projects</a>
+                    <a className="pb-1 hover:border-b-2 hover:border-gray-900 hover:font-semibold dark:hover:border-gray-100 dark:hover:text-gray-100">
+                      Projects
+                    </a>
                   </Link>
                 </li>
-                <li className="py-3 text-center hover:font-semibold dark:hover:text-gray-100 md:py-1 lg:text-left">
+                <li className="py-3 text-center md:py-1 lg:text-left">
                   <Link href="/posts">
-                    <a className="pb-1 hover:border-b-2 hover:border-gray-900 dark:hover:border-gray-100">Blog</a>
+                    <a className="pb-1 hover:border-b-2 hover:border-gray-900 hover:font-semibold dark:hover:border-gray-100 dark:hover:text-gray-100">
+                      Blog
+                    </a>
                   </Link>
                 </li>
-                <li className="py-3 text-center hover:font-semibold dark:hover:text-gray-100 md:py-1 lg:text-left">
+                <li className="py-3 text-center md:py-1 lg:text-left">
                   <Link href="/snippets">
-                    <a className="pb-1 hover:border-b-2 hover:border-gray-900 dark:hover:border-gray-100">Snippets</a>
+                    <a className="pb-1 hover:border-b-2 hover:border-gray-900 hover:font-semibold dark:hover:border-gray-100 dark:hover:text-gray-100">
+                      Snippets
+                    </a>
                   </Link>
                 </li>
               </ul>
             </nav>
           </section>
-          <section className="lg:col-span-2">
-            <div className="px-2 pb-2 text-center text-xs lg:text-left">All rights reserved © Guanghui Wang 2022</div>
+          <section className="lg:pl-[50%]">
+            <div className="whitespace-nowrap pb-2 text-center text-xs lg:text-left">
+              All rights reserved © Guanghui Wang 2022
+            </div>
           </section>
         </div>
       </div>
