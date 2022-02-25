541465d68ab8625976f22131164b093a35ecbd14
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-25 21:01:56 +0800

    style: update card style

--- components/share/AppCard.tsx
+++ components/share/AppCard.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import {useRouter} from 'next/router';
 import {ReactNode} from 'react';
+import {MdOutlineAccessTime} from 'react-icons/md';
 
 export default function AppCard({
   title,
@@ -27,15 +28,20 @@ export default function AppCard({
       {children}
       <div className="flex flex-1 flex-col gap-4 px-8 py-6 shadow-xs">
         <div className="flex-1 space-y-6">
-          <header className="text-2xl font-semibold group-hover:text-king-500 lg:text-center">
+          <header className="text-2xl font-semibold group-hover:text-king-500">
             <Link href={href}>
               <a className="inline-block w-full">{title}</a>
             </Link>
           </header>
           <p className="text-gray-500 dark:text-gray-400">{excerpt}</p>
         </div>
-        <time className="block font-serif text-xs text-gray-500 dark:text-gray-400">
-          {new Date(date).toLocaleDateString()}
+        <time className="flex items-center gap-2 text-sm text-gray-500 dark:text-gray-400">
+          <MdOutlineAccessTime size="18" />
+          {new Date(date).toLocaleDateString('en-us', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+          })}
         </time>
       </div>
     </div>
