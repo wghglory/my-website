d0c6d10212d137fb879565b9c69c53951c7bf986
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-27 19:25:24 +0800

    refactor: use DrAspectRatio

--- components/share/CodeFrame.tsx
+++ components/share/CodeFrame.tsx
@@ -1,6 +1,7 @@
 // Refer content/snippets/aspect-ratio.mdx
 // Refer https://htmldom.dev/show-a-loading-indicator-when-an-iframe-is-being-loaded/
 
+import {DrAspectRatio} from '@wghglory/dr-components';
 import {useEffect, useRef} from 'react';
 import {FaSpinner} from 'react-icons/fa';
 
@@ -21,9 +22,9 @@ export default function CodeFrame({src, title = 'code demo'}: {src: string; titl
   }, []);
 
   return (
-    <div className="relative my-16 h-0 max-w-full overflow-hidden pb-[56.25%]">
+    <DrAspectRatio ratio="16:9" containerClass="my-16">
       {/* Can further make a animated svg loading icon */}
-      <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center" ref={loadingRef}>
+      <div className="flex items-center justify-center" ref={loadingRef}>
         Loading Code Editor
         <FaSpinner className="ml-4 animate-spin" />
       </div>
@@ -35,8 +36,8 @@ export default function CodeFrame({src, title = 'code demo'}: {src: string; titl
         sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
         allow="accelerometer; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; xr-spatial-tracking"
         title={title}
-        className="absolute inset-0 h-full w-full"
+        className="inset-0"
       />
-    </div>
+    </DrAspectRatio>
   );
 }
