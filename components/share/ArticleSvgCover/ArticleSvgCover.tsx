72271b048964718bf8c275a32b7dd0e7aabfbea9
Guanghui Wang <guanghui-wang@foxmail.com>
2022-02-27 23:49:13 +0800

    feat: use gloryui-react

--- components/share/ArticleSvgCover/ArticleSvgCover.tsx
+++ components/share/ArticleSvgCover/ArticleSvgCover.tsx
@@ -7,32 +7,6 @@ import TopicTitle from './ArticleTitle';
 export default function ArticleSvgCover({title, subtitle, topic}: {title: string; subtitle: string; topic: TopicType}) {
   return (
     <svg width="100%" viewBox="0 0 400 225" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <style jsx>
-        {`
-          .title-container {
-            height: 100%;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            gap: 20px;
-          }
-
-          .title {
-            font-size: 44px;
-            text-align: center;
-          }
-
-          .subtitle {
-            font-size: 24px;
-            text-align: center;
-            color: white;
-            font-weight: 500;
-            font-family: Arial;
-          }
-        `}
-      </style>
-
       <foreignObject x="0" y="0" width="400" height="225">
         <div className="flex h-full w-full items-center justify-center gap-4 bg-gradient-to-b from-black via-gray-900 to-gray-700 duration-300 group-hover:scale-105">
           <div className="w-32">
