236b03199183729e8dbbc2485c83125fc5714ffe
Guanghui Wang <guanghui-wang@foxmail.com>
2022-03-21 20:00:41 +0800

    style: add motion for logo

--- lib/motion.ts
+++ lib/motion.ts
@@ -1,6 +1,6 @@
 export const svgVariants = {
   start: {opacity: 0, pathLength: 0},
-  end: {opacity: 1, pathLength: 1, transition: {duration: 1, ease: 'easeInOut'}},
+  end: {opacity: 1, pathLength: 1, transition: {duration: 2, ease: 'easeInOut'}},
 };
 
 export const fadeInVariant = {
