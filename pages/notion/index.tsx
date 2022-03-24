81236a9d0f766275b05f40e7430b48826c847fc7
Guanghui Wang <guanghui-wang@foxmail.com>
2022-03-24 10:30:58 +0800

    fix: build issue

--- pages/notion/index.tsx
+++ pages/notion/index.tsx
@@ -1,11 +1,8 @@
-import dynamic from 'next/dynamic';
 import {ExtendedRecordMap} from 'notion-types';
-import React from 'react';
 
+import NotionPage from '@/components/share/NotionPage';
 import {getPage} from '@/lib/notion';
 import {previewImagesEnabled, rootDomain, rootNotionPageId} from '@/lib/notion/config';
-
-const NotionPage = dynamic(() => import('@/components/share/NotionPage'), {ssr: false});
 
 export const getStaticProps = async () => {
   // 9bdcd90897794450a0a433b70eeb9c01 database id
