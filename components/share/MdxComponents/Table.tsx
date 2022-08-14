45af012d80ecc41c605480492c184397b827acbd
Guanghui Wang <guanghui-wang@foxmail.com>
2022-08-14 22:35:08 +0800

    docs: structural directive

---
+++ components/share/MdxComponents/Table.tsx
@@ -0,0 +1,22 @@
+export default function Table({columns, data}: {columns: string[]; data: React.ReactNode[][]}) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {columns.map((column, index) => (
+            <th key={index}>{column}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            {row.map((cell, index) => (
+              <td key={index}>{cell}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
--- components/share/MdxComponents/index.tsx
+++ components/share/MdxComponents/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Bilibili from './Bilibili';
 import CodeFrame from './CodeFrame';
 import Quote from './Quote';
+import Table from './Table';
 import YouTube from './Youtube';
 
 export const mdxComponents = {
@@ -11,4 +12,5 @@ export const mdxComponents = {
   CodeFrame,
   Quote,
   Image,
+  Table,
 };
